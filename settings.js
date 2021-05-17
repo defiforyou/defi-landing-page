6e63d10f4e969aa3d134e8ce74959a9bd3a6c317
coadotvn <gia@coa.vn>
2021-05-18 01:04:14 +0700

    sean description updated

--- settings.js
+++ settings.js
@@ -87,106 +87,116 @@ export const PARTNERS = [{
   exchange: true
 }]
 
-export const TEAM_MEMBERS = [{
-  position: 'CEO & Founder',
-  image: '/img/team.adam-c-chaplin.jpg',
-  name: 'Adam Chaplin',
-  bio: 'Adam was a co-founder of Travala (AVA) and is a former Royal Marine Commando. His decades of business and military experience make him a capable and reliable leader.',
-  other: {
-    width: 100,
-    image: '/img/logo.travala.png',
-    name: 'Travala',
-    link: 'https://www.travala.com/'
+export const TEAM_MEMBERS = [
+  {
+    position: 'CEO & Founder',
+    image: '/img/team.adam-c-chaplin.jpg',
+    name: 'Adam Chaplin',
+    bio: 'Adam was a co-founder of Travala (AVA) and is a former Royal Marine Commando. His decades of business and military experience make him a capable and reliable leader.',
+    other: {
+      width: 100,
+      image: '/img/logo.travala.png',
+      name: 'Travala',
+      link: 'https://www.travala.com/'
+    },
+    email: '',
+    facebook: 'https://www.facebook.com/adamchaplin1968',
+    linkedin: 'https://www.linkedin.com/in/adamchristopherchaplin/'
   },
-  email: '',
-  facebook: 'https://www.facebook.com/adamchaplin1968',
-  linkedin: 'https://www.linkedin.com/in/adamchristopherchaplin/'
-}, {
-  position: 'CTO',
-  image: '/img/team.tuan-dang-minh.jpg',
-  name: 'Dr. Tuan Dang Minh',
-  bio: 'Dr. Tuan was the inventor of Vietkey and is a P.h.D in Math and Cryptography. He\'s one of the leading blockchain experts in South East Asia.',
-  other: {
-    width: 64,
-    image: '/img/logo.vietkey.png',
-    name: 'Vietkey',
-    link: 'https://vi.wikipedia.org/wiki/Vietkey'
+  {
+    position: 'CTO',
+    image: '/img/team.tuan-dang-minh.jpg',
+    name: 'Dr. Tuan Dang Minh',
+    bio: 'Dr. Tuan was the inventor of Vietkey and is a P.h.D in Math and Cryptography. He\'s one of the leading blockchain experts in South East Asia.',
+    other: {
+      width: 64,
+      image: '/img/logo.vietkey.png',
+      name: 'Vietkey',
+      link: 'https://vi.wikipedia.org/wiki/Vietkey'
+    },
+    email: '',
+    facebook: 'https://www.facebook.com/tuanvietkey',
+    linkedin: 'https://www.linkedin.com/in/dang-minh-tuan-a3b32551/'
   },
-  email: '',
-  facebook: 'https://www.facebook.com/tuanvietkey',
-  linkedin: 'https://www.linkedin.com/in/dang-minh-tuan-a3b32551/'
-}, {
-  position: 'CEO of Blockchain Developer Asia',
-  image: '/img/team.cong-nguyen.jpg',
-  name: 'Cong Nguyen',
-  bio: 'Cong is developing the DeFi For You. platform, as well as blockchain projects for the Vietnamese government, banks, and industry.',
-  other: {
-    width: 180,
-    image: '/img/logo.blockchain-developer-asia.png',
-    name: 'Blockchain Developer Asia',
-    link: 'https://blockchaindeveloper.asia'
+  {
+    position: 'CEO of Blockchain Developer Asia',
+    image: '/img/team.cong-nguyen.jpg',
+    name: 'Cong Nguyen',
+    bio: 'Cong is developing the DeFi For You. platform, as well as blockchain projects for the Vietnamese government, banks, and industry.',
+    other: {
+      width: 180,
+      image: '/img/logo.blockchain-developer-asia.png',
+      name: 'Blockchain Developer Asia',
+      link: 'https://blockchaindeveloper.asia'
+    },
+    email: '',
+    facebook: 'https://www.facebook.com/cong.nguyen.1614',
+    linkedin: 'https://www.linkedin.com/in/cong-nguyen-b5b274102'
   },
-  email: '',
-  facebook: 'https://www.facebook.com/cong.nguyen.1614',
-  linkedin: 'https://www.linkedin.com/in/cong-nguyen-b5b274102'
-}, {
-  position: 'Strategic Partner',
-  image: '/img/team.sean-mason.jpg',
-  name: 'Sean Mason',
-  bio: 'Sean is a world-class business coach and strategic partner of DeFi For You, based in the UK.',
-  other: null,
-  email: '',
-  facebook: 'https://www.facebook.com/SeanMasonKiani',
-  linkedin: null
-}, {
-  position: 'Tech Team Leader',
-  image: '/img/team.tung-duong-thanh.jpg',
-  name: 'Tung Duong Thanh',
-  bio: 'Tung was the tech team leader of Travala and is now heading up the troops at DeFi For You. He\'s a passionate developer and leader.',
-  other: {
-    width: 100,
-    image: '/img/logo.travala.png',
-    name: 'Travala',
-    link: 'https://www.travala.com/'
+  {
+    position: 'CFO',
+    image: '/img/team.sean-mason.jpg',
+    name: 'Sean Mason',
+    bio: 'Seasoned business veteran of 20+ years and owner of bankruptcy.co.uk',
+    other: null,
+    email: '',
+    facebook: 'https://www.facebook.com/SeanMasonKiani',
+    linkedin: null
   },
-  email: '',
-  facebook: 'https://www.facebook.com/Socoboy',
-  linkedin: 'https://www.linkedin.com/in/d%C6%B0%C6%A1ng-thanh-t%C3%B9ng-845aa764/'
-}, {
-  position: 'CMO',
-  image: '/img/team.william-gray.jpg',
-  name: 'William Gray',
-  bio: 'William is a digital marketing specialist and copywriter with a background working with eCommerce app developers, blockchain projects, and a national newspaper.',
-  other: null,
-  phone: '+84395340013',
-  email: '',
-  facebook: 'https://www.facebook.com/billygraywriter/',
-  linkedin: 'https://www.linkedin.com/in/billygraywriter/'
-}, {
-  position: 'CFO',
-  image: '/img/team.hoang-dinh-ngoc.jpg',
-  name: 'Hoang Dinh Ngoc',
-  bio: 'Hoang\'s experience in accounting and management are demonstrated daily in his role as Chief Financial Officer at DeFi For You.',
-  other: null,
-  email: '',
-  facebook: '',
-  linkedin: ''
-}, {
-  position: 'Vice Director of Internal Audit, VietBank',
-  image: '/img/team.hoang-dung.jpg',
-  name: 'Dung Hoang',
-  bio: 'Dung\'s role as an advisor helps us to self-audit the project, alongside having third-parties like CertiK audit us, to ensure we\'re on track.',
-  other: null,
-  email: '',
-  facebook: '',
-  linkedin: ''
-}, {
-  position: 'Pawnbroker Partner',
-  image: '/img/team.miles-mann.jpg',
-  name: 'Miles Mann',
-  bio: 'Founder and MD of Cotswold Fine Jewellery Group and a fellow of the Gemmological Association of Great Britain, a member of the National Association of Goldsmiths, and the National Pawnbrokers Association.',
-  other: null,
-  email: '',
-  facebook: '',
-  linkedin: ''
-}]
+  {
+    position: 'Tech Team Leader',
+    image: '/img/team.tung-duong-thanh.jpg',
+    name: 'Tung Duong Thanh',
+    bio: 'Tung was the tech team leader of Travala and is now heading up the troops at DeFi For You. He\'s a passionate developer and leader.',
+    other: {
+      width: 100,
+      image: '/img/logo.travala.png',
+      name: 'Travala',
+      link: 'https://www.travala.com/'
+    },
+    email: '',
+    facebook: 'https://www.facebook.com/Socoboy',
+    linkedin: 'https://www.linkedin.com/in/d%C6%B0%C6%A1ng-thanh-t%C3%B9ng-845aa764/'
+  },
+  {
+    position: 'CMO',
+    image: '/img/team.william-gray.jpg',
+    name: 'William Gray',
+    bio: 'William is a digital marketing specialist and copywriter with a background working with eCommerce app developers, blockchain projects, and a national newspaper.',
+    other: null,
+    phone: '+84395340013',
+    email: '',
+    facebook: 'https://www.facebook.com/billygraywriter/',
+    linkedin: 'https://www.linkedin.com/in/billygraywriter/'
+  },
+  // {
+  //   position: 'CFO',
+  //   image: '/img/team.hoang-dinh-ngoc.jpg',
+  //   name: 'Hoang Dinh Ngoc',
+  //   bio: 'Hoang\'s experience in accounting and management are demonstrated daily in his role as Chief Financial Officer at DeFi For You.',
+  //   other: null,
+  //   email: '',
+  //   facebook: '',
+  //   linkedin: ''
+  // },
+  {
+    position: 'Vice Director of Internal Audit, VietBank',
+    image: '/img/team.hoang-dung.jpg',
+    name: 'Dung Hoang',
+    bio: 'Dung\'s role as an advisor helps us to self-audit the project, alongside having third-parties like CertiK audit us, to ensure we\'re on track.',
+    other: null,
+    email: '',
+    facebook: '',
+    linkedin: ''
+  },
+  {
+    position: 'Pawnbroker Partner',
+    image: '/img/team.miles-mann.jpg',
+    name: 'Miles Mann',
+    bio: 'Founder and MD of Cotswold Fine Jewellery Group and a fellow of the Gemmological Association of Great Britain, a member of the National Association of Goldsmiths, and the National Pawnbrokers Association.',
+    other: null,
+    email: '',
+    facebook: '',
+    linkedin: ''
+  }
+]
