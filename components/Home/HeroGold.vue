60a231b9cfce83d9af0fc000f7e1cb90040d635c
quanghung97 <hungcan1997@gmail.com>
2022-06-15 13:40:08 +0700

    fix text

--- components/Home/HeroGold.vue
+++ components/Home/HeroGold.vue
@@ -16,18 +16,18 @@
               font-size: 32px;
               line-height: 32px;
               text-align: center;
-
+              margin-bottom: 60px;
               color: #FFFFFF;
         "
       >
         Transfer your crypto and wealth into gold bullion during the crypto bear market.
       </div>
-      <div
+      <!-- <div
         style="font-family: 'Montserrat';
               font-style: normal;
               font-weight: 500;
-              font-size: 32px;
-              line-height: 32px;
+              font-size: 16px;
+              line-height: 20px;
               text-align: center;
 
               color: #FFFFFF;
@@ -39,15 +39,15 @@
         style="font-family: 'Montserrat';
               font-style: normal;
               font-weight: 500;
-              font-size: 32px;
-              line-height: 32px;
+              font-size: 16px;
+              line-height: 20px;
               text-align: center;
               margin-bottom: 60px;
               color: #FFFFFF;
         "
       >
         Hold gold until the crypto market corrects itself.
-      </div>
+      </div> -->
 
       <!-- <div style="text-align: center; margin-top: 33px; margin-bottom: 40px;">
         <v-btn
